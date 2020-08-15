13517aa4dc248aca606647b968ef23bb6871e066
EC2 Default User <ec2-user@ip-172-31-43-12.ap-northeast-2.compute.internal>
2020-08-15 07:37:40 +0000

    avgpredict img list 가져오기 완료

--- routes/api/emotion/emotion.controller.js
+++ routes/api/emotion/emotion.controller.js
@@ -1,14 +1,16 @@
 const model = require('../../../models');
 const fs = require('fs');
 
+
+
 exports.predict = async (req,res,next) => {
     const { PythonShell } = require("python-shell");
-    let image_data = "/home/ec2-user/app/what/EatTogether_Server/emotion/picture1.jpg";
+    let image_data = "/home/ec2-user/app/what/EatTogether_Server/routes/api/emotion/img/picture1.jpg";
 
     let options = {
         mode: 'text',
         pythonPath: "/usr/bin/python3",
-        scriptPath: "/home/ec2-user/app/what/EatTogether_Server/emotion",
+        scriptPath: "/home/ec2-user/app/what/EatTogether_Server/routes/api/emotion",
         pythonOptions: ['-u'],
         args: [image_data]
     };
@@ -16,7 +18,7 @@ exports.predict = async (req,res,next) => {
     const result = await new Promise((resolve, reject) => {
         PythonShell.run('prediction.py', options, function(err, data) {
             if (err) return reject(err);
-            console.log('prediction: %s', resolve(data));
+            //console.log('prediction: %s', resolve(data));
         });
     });
     
@@ -40,10 +42,40 @@ exports.saveimage = async (req,res,next) => {
         var buffer = new Buffer.from(img, 'base64');
         console.log("dir: "+__dirname);
     
-        var filename = '/home/ec2-user/app/what/EatTogether_Server/routes/api/emotion/img/'+deviceNum+'+'+imgOrder + rand_str + '.jpg';
+        var filename = '/home/ec2-user/app/what/EatTogether_Server/routes/api/emotion/img/'+deviceNum+'+'+imgOrder+'+'+ rand_str + '.jpg';
         console.log(filename);
+    
         fs.writeFileSync(filename, buffer, function(err){
             console.log(err);
         });
+}
+
+exports.avgpredict = async (req,res) => {
+    var deviceNum= req.body.deviceNum;
+    var imgOrder = req.body.imgOrder;
+    var array1 = [];
+    var str = deviceNum + '+'+imgOrder;
+    
+    const path = require('path');
+    const directoryPath = path.join(__dirname, 'img');    
+
+    const result = await new Promise((resolve, reject) => {
+        fs.readdir(directoryPath, function (err, files) {
+            console.log(files);
+            files.forEach(async (file) => {
+                if(file.indexOf(str)>=0){
+                    console.log(file);
+                    await array1.push(file);
+                    //console.log("in "+array1);
+                }
+            });
+
+            console.log("in "+array1);
+            return resolve(array1);
+        });
+    });
+    
+    console.log("out "+result);
+    console.log(typeof result);
 }
 
--- routes/api/emotion/prediction.py
+++ routes/api/emotion/prediction.py
@@ -5,11 +5,22 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import model_from_json
 import sys
 
+#model = keras.models.load_model('/home/ec2-user/app/what/EatTogether_Server/emotion/model.h5')
 
 
-picture = sys.argv[1]
+#def loadImage(filepath):
+#    test_img = image.load_img(filepath, target_size=(224, 224))
+#    test_img = image.img_to_array(test_img)
+#    test_img = np.expand_dims(test_img, axis = 0)
+#    test_img /= 255
+#    return test_img
+
+#picture = sys.argv[1]
 #prediction = model.predict(loadImage(picture))
 
 
-print("happy")
 
+
+#emotion = "Happy!" if np.argmax(prediction) == 1 else "Neutral"
+
+print("happy")
