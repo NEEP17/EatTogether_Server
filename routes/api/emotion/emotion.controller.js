ee6343e99b9d9bb22259cb66bceb7f36b5675479
EC2 Default User <ec2-user@ip-172-31-43-12.ap-northeast-2.compute.internal>
2020-08-25 06:26:36 +0000

    ranking socket 완료

--- routes/api/emotion/emotion.controller.js
+++ routes/api/emotion/emotion.controller.js
@@ -1,9 +1,5 @@
-const model = require('../../../models');
-const fs = require('fs');
-
-
 // predict function
-var predict = async function (image_data){
+module.exports.predict = async function (image_data){
     const { PythonShell } = require("python-shell");
     var image_data = image_data;
 
@@ -26,68 +22,3 @@ var predict = async function (image_data){
     console.log("result: "+result);
     return result;
 }
-
-exports.saveimage = async (req,res,next) => {
-        var img = req.body.img;
-        var deviceNum = req.body.deviceNum;
-        var imgOrder = req.body.imgOrder;
-    
-        var rand = Math.floor(100 + Math.random() * 900);
-        console.log("rand"+rand);
-        
-        var rand_str = rand.toString();
-        console.log("rand_str"+rand_str)
-        // decode
-        var buffer = new Buffer.from(img, 'base64');
-        console.log("dir: "+__dirname);
-    
-        var filename = '/home/ec2-user/app/what/EatTogether_Server/routes/api/emotion/img/'+deviceNum+'+'+imgOrder+'+'+ rand_str + '.jpg';
-        console.log(filename);
-    
-        fs.writeFileSync(filename, buffer, function(err){
-            console.log(err);
-        });
-}
-
-exports.avgpredict = async (req,res) => {
-    var deviceNum= req.body.deviceNum;
-    var imgOrder = req.body.imgOrder;
-    var array1 = [];
-    var str = deviceNum + '+'+imgOrder;
-    var pred_list = [];
-    var sum = 0;
-    
-    const path = require('path');
-    const directoryPath = path.join(__dirname, 'img');    
-
-    
-    // img list 가져오기
-    
-    const img_list = await new Promise((resolve, reject) => {
-        fs.readdir(directoryPath, function (err, files) {
-            files.forEach(async (file) => {
-                if(file.indexOf(str)>=0){
-                    await array1.push(file);
-                }
-            });
-            return resolve(array1);
-        });
-    });
-    
-    // for len(predict)
-    for(var i=0; i<img_list.length; i++){
-        var str_tmp = directoryPath +"/"+img_list[i];
-        img_list[i] = str_tmp;
-        console.log("img_list: "+img_list[i]);
-        sum += Number(await predict(img_list[i]));
-    }
-        
-    // avg -> db.save
-    var avg = sum/3;
-    await model.Room.updateOne(
-        {"deviceNum" : deviceNum},
-        {"$push": {"pred": [avg]} } 
-    );
-    
-}
-
--- routes/api/socket.js
+++ routes/api/socket.js
@@ -1,4 +1,6 @@
 const model = require('../../models');
+const fs = require('fs');
+const emotionController = require('./emotion/emotion.controller');
 const flag_success = 200;
 const flag_fail = 400;
 
@@ -66,7 +68,7 @@ module.exports = ranking => {
             var bad = bad;
             var deviceNum = deviceNum;
             var roomID = roomID;
-            var count;
+            var totalCount;
             
             console.log("good: "+good+" bad: "+bad+ " roomID: "+ roomID + " deviceNum: " +deviceNum);
             
@@ -74,9 +76,9 @@ module.exports = ranking => {
             await model.RoomCheck.findOne({
                 roomID: roomID
             }).then ((result) => {
-                count = result.count;
+                totalCount = result.count;
             });
-            console.log("count: "+count);
+            console.log("totalCount: "+totalCount);
 
             //var manager = ranking.adapter ? { 
             //    rooms : ranking.adapter.rooms,  
@@ -87,72 +89,63 @@ module.exports = ranking => {
             
             // 선호도 저장 및 인원 체크 후 client에게 넘겨주기
             try{
-                await model.Room.findOne({
-                    deviceNum: deviceNum
-                }).then(async (result) =>{
-                    // 방 아이디가 있으면..
-                    if(result){
-                        // 선호도 저장
-                        await model.Room.update({
-                            good: good,
-                            bad: bad
-                        }).then(async (success)=>{
-                             if (!success) {
-                                console.log("선호도 저장 실패");
-                                 ranking.to(socket.id).emit("error");
-                             } else {
-                                 console.log("선호도 저장 성공");
-                                // 인원 수 저장
-                                //ranking.adapter.rooms[roomID].userList.push(deviceNum);
-                                 whoisReady.push(deviceNum);
+                // deviceNum이 방에 들어와 있으면..
+                
+                // 선호도 저장
+                await model.Room.updateOne(
+                    { deviceNum: deviceNum },
+                    { $set: { good: good , bad: bad} }
+                ).then(async (success)=>{
+                     if (!success) {
+                        console.log("선호도 저장 실패");
+                         ranking.to(socket.id).emit("error");
+                     } else {
+                         console.log("선호도 저장 성공");
+                        // 인원 수 저장
+                        //ranking.adapter.rooms[roomID].userList.push(deviceNum);
+                         whoisReady.push(deviceNum); // 배열에 같은 deviceNum이 들어가지 않도록 유지보수..
 
-                                // 현재 선호도 입력 한 인원수
-                                //var cntCurUsers = ranking.adapter.rooms[roomID].userList.length();
-                                 var cntCurUsers = whoisReady.length;
-                                 console.log("cntCurUsers: "+cntCurUsers);
-                                // 현재 대기 인원 수 넘겨주기
-                                if(cntCurUsers !== count){
-                                    console.log("if 문 안으로 들어옴");
-                                    ranking.emit('currentCount', cntCurUsers);
-                                }// 모든 인원이 선호도 입력 완료 시 음식 리스트 넘겨주기
-                                 else{
-                                     // cf 이용해서 리스트 생성
-                                     var food_list = [];
-                                        var tmp = 0;
-                                        while(true){
-                                            try{
-                                                await model.Food.count({}).then(async (count) => {
-                                                    if(count) {
-                                                        var random = Math.floor(Math.random() * count)
-                                                        console.log(count);
-                                                        await model.Food.findOne({}).skip(random).then(async(result)=> {
-                                                            food_list.push(await result);
-                                                            tmp += 1;
-                                                            console.log(tmp);
-                                                        });
-                                                    }
-                                                }).catch((err) => {
-                                                    console.log(err);
+                        // 현재 선호도 입력 한 인원수
+                        //var cntCurUsers = ranking.adapter.rooms[roomID].userList.length();
+                         var cntCurUsers = whoisReady.length;
+                         console.log("cntCurUsers: "+cntCurUsers);
+                        // 현재 대기 인원 수 넘겨주기
+                        if(cntCurUsers !== 1){
+                            console.log("if 문 안으로 들어옴");
+                            ranking.emit('currentCount', cntCurUsers,totalCount);
+                        }// 모든 인원이 선호도 입력 완료 시 음식 리스트 넘겨주기
+                         else{
+                             // cf 이용해서 리스트 생성
+                             var food_list = [];
+                                var tmp = 0;
+                                while(true){
+                                    try{
+                                        await model.Food.count({}).then(async (count) => {
+                                            if(count) {
+                                                var random = Math.floor(Math.random() * count)
+                                                console.log(count);
+                                                await model.Food.findOne({}).skip(random).then(async(result)=> {
+                                                    food_list.push(await result);
+                                                    tmp += 1;
+                                                    console.log(tmp);
                                                 });
-                                            } catch (Error) {
-                                                console.log(Error);
-                                                ranking.to(socket.id).emit("error");
                                             }
-                                            if(tmp===10){
-                                                break;
-                                            } 
-                                        }
-                                        console.log(food_list);
-                                    ranking.emit('finishPref', food_list);
+                                        }).catch((err) => {
+                                            console.log(err);
+                                        });
+                                    } catch (Error) {
+                                        console.log(Error);
+                                        ranking.to(socket.id).emit("error");
+                                    }
+                                    if(tmp===10){
+                                        break;
+                                    } 
                                 }
-                            }
-                        })
-                    }else{
-                        // 방이 생성되어 있지 않음을 client에게 에러 전송
-                        console.log("방이 생성되어 있지 않음");
-                        ranking.to(socket.id).emit("error");
+                                console.log(food_list);
+                            ranking.emit('finishPref', food_list);
+                        }
                     }
-                });
+                });                
             } catch(err){
                 console.log(err);
             }    
@@ -185,39 +178,96 @@ module.exports = ranking => {
             });
             
         });
+
         
         // Cient가 3초마다 event 발생하면 감정 predict
-        socket.on('avgpredict', async(deviceNum, imgOrder) => {
-            var deviceNum = deviceNum;
+        socket.on('avgPredict', async(deviceNum, imgOrder) => {
+            var deviceNum= deviceNum;
             var imgOrder = imgOrder;
+            var array1 = [];
+            var str = deviceNum + '+' + imgOrder;
+            var pred_list = [];
+            var sum = 0;
+
+            const path = require('path');
+            const directoryPath = path.join(__dirname, 'img');    
+
+
+            // img list 가져오기
+
+            const img_list = await new Promise((resolve, reject) => {
+                fs.readdir(directoryPath, function (err, files) {
+                    files.forEach(async (file) => {
+                        if(file.indexOf(str)>=0){
+                            await array1.push(file);
+                        }
+                    });
+                    return resolve(array1);
+                });
+            });
+
+            // for len(predict)
+            for(var i=0; i<img_list.length; i++){
+                var str_tmp = directoryPath +"/"+img_list[i];
+                img_list[i] = str_tmp;
+                console.log("img_list: "+img_list[i]);
+                sum += Number(await emotionController.predict(img_list[i]));
+            }
+
+            // avg -> db.save
+            var avg = sum/3;
+            await model.Room.updateOne(
+                {"deviceNum" : deviceNum},
+                {"$push": {"pred": [avg]} } 
+            );
             
-            // deviceNum, imgOrder과 일치하는 사진 3개 find해서 list에 넣기.
-            
-            // list for문 돌려서 각각 predict하기
-            // avg three predictions -> db.save
-           
-        });
-        
-        /*
-        socket.on('wait', data => {
-            controller.writeMessage(data.good, data.bad);
-            console.log("save good, bad menu");
-            socket.to(room).emit('changeTopic', data);
-        });
-
-        */
-
-        /*
-        socket.on('disconnect', data => {
-            console.log('disconnect from room: ', socket.room);
-            rooms_users[socket.room].pop();
-
-            // if nobody remain
-            if (rooms_users[socket.room].length === 0) {
-                controller.endLogging(socket.room);
+            // 마지막 이미지까지 들어왔으면..
+            if(imgOrder==9){
+                ranking.emit('finishPred')
             }
         });
-        */
+        
+        // 지윤 위한 랭킹 소켓 간단히 완료        
+        socket.on('showRank', async(roomID) => {            
+            // 랭킹 알고리즘
+            
+            
+            
+            // 랭킹 리스트를 emit
+            var roomID = roomID;
+            
+            var ranking_list = [];
+            var tmp = 0;
+            while(true){
+                try{
+                    await model.Food.count({}).then(async (count) => {
+                        if(count) {
+                            var random = Math.floor(Math.random() * count)
+                            console.log(count);
+                            await model.Food.findOne({}).skip(random).then(async(result)=> {
+                                ranking_list.push(await result);
+                                tmp += 1;
+                                console.log(tmp);
+                            });
+                        }
+                    }).catch((err) => {
+                        console.log(err);
+                    });
+                } catch (Error) {
+                    console.log(Error);
+                    ranking.to(socket.id).emit("error");
+                }
+                if(tmp===10){
+                    break;
+                } 
+            }
+            
+            console.log("showRank:"+roomID);
+            
+            ranking.emit('finishRank', ranking_list);
+            console.log("ranking_list:"+ranking_list);
+        });
+        
     });
 }
 
