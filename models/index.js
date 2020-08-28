d35c91b261da9d8a6d42cc5f8406836d7ded95d9
EC2 Default User <ec2-user@ip-172-31-43-12.ap-northeast-2.compute.internal>
2020-08-28 04:44:18 +0000

    mongodb 환경변수 적용

--- models/index.js
+++ models/index.js
@@ -3,7 +3,8 @@
 const mongoose    = require('mongoose');
 const env = process.env.NODE_ENV || 'development';
 const db = {};
-
+const { MONGO_ID , MONGO_PASSWORD } = process.env;
+const MONGO_URL = `mongodb://${MONGO_ID}:${MONGO_PASSWORD}@localhost:27017/neep?authSource=admin`;
 // [ CONFIGURE mongoose ]
 // CONNECT TO MONGODB SERVER
 var mongo = mongoose.connection;
@@ -11,7 +12,7 @@ mongo.on('error', console.error.bind(console, 'connection error:'));
 mongo.once('open', function callback () {
     console.log("mongo db connection OK.");
 });
-mongoose.connect('mongodb://neep:NeepWhat!@localhost:27017/neep?authSource=admin');
+mongoose.connect(MONGO_URL);
 
 db.mongoose = mongoose;
 db.Room = require('./room')(db.mongoose);
@@ -19,5 +20,6 @@ db.RoomCheck = require('./roomcheck')(db.mongoose);
 db.Food = require('./food')(db.mongoose);
 
 module.exports = db;
+
 
 
